07263ab5e174ee92fc83cdbebcd2c1464ebb8125
mark_skidmore <mark.skidmore@autopoint.com>
2018-06-07 11:22:02 -0600

    Initial check-in of MobileBackendController and models

---
+++ ADXETools/Controllers/MobileBackendController.cs
@@ -0,0 +1,107 @@
+﻿using ADXETools.FalconRequests;
+using ADXETools.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ADXETools.Controllers
+{
+    [Produces("application/xml", "application/json")]
+    [Consumes("application/xml", "application/json")]
+    public class MobileBackEndController : Controller
+    {
+        readonly IFalconPort falconPort;
+        const string aspPage = "MobileBackEndSVR.asp";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="falconPort"></param>
+        public MobileBackEndController(IFalconPort falconPort)
+        {
+            this.falconPort = falconPort;
+        }
+
+        /// <summary>
+        /// Adds attachment from mobile device
+        /// </summary>
+        /// <param name="xmlInput"></param>
+        /// <returns></returns>
+        /// <response code = "201">Returns saved attachment info</response>
+        /// <response code = "400">Invalid input parameters</response>
+        /// <response code = "401">Active authorization is missing</response>
+        /// <response code = "1000">Work_Assignment ID Missing</response>
+        /// <response code = "2000">Role Code Missing</response>
+        /// <response code = "3000">usiness Category Missing Missing</response>
+        /// <response code = "4000">Invalid Ticket</response>
+        //[SwaggerRequestExample(typeof(string),typeof(OONLookupVehicleExample))]
+        [HttpPost("MobileBackEndAddAttachment")]
+        [ProducesResponseType(typeof(MBESVRAddAttachmentRequest), 201)]
+        public async Task<IActionResult> MobileBackEndAddAttachment([FromBody]MBESVRAddAttachmentRequest xmlInput)
+        {
+            try
+            {
+                if (xmlInput == null)
+                {
+                    string msg = string.Format("Invalid input data received <{0}>.  Verify input request data.", xmlInput);
+                    return BadRequest(msg);
+                }
+                var xmlOutput = xmlInput.ToXml("Request");//await falconPort.SubmitFalconRequest(aspPage, xmlInput);
+                return StatusCode(StatusCodes.Status201Created, xmlOutput);
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format("Processing error <{0}> received for PostAttachments request <{1}>.  Verify input request data.", ex.Message, xmlInput);
+                return BadRequest(msg);
+            }
+        }
+        /// <summary>
+        /// Adds event from mobile device
+        /// </summary>
+        [HttpPost("MobileBackEndAddAttEvent")]
+        [ProducesResponseType(typeof(MBESVRAddAttachmentRequest), 201)]
+        public async Task<IActionResult> MobileBackEndAddAttEvent([FromBody]MBESVRAddAttEventRequest xmlInput)
+        {
+            try
+            {
+                if (xmlInput == null)
+                {
+                    string msg = string.Format("Invalid input data received <{0}>.  Verify input request data.", xmlInput);
+                    return BadRequest(msg);
+                }
+                var xmlOutput = xmlInput.ToXml("Request");//await falconPort.SubmitFalconRequest(aspPage, xmlInput);
+                return StatusCode(StatusCodes.Status201Created, xmlOutput);
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format("Processing error <{0}> received for PostAttachments request <{1}>.  Verify input request data.", ex.Message, xmlInput);
+                return BadRequest(msg);
+            }
+        }
+
+        /// <summary>
+        /// Updates a vehicle from a mobile device
+        /// </summary>
+        [HttpPost("MobileBackEndUpdateVehicle")]
+        [ProducesResponseType(typeof(MBESVRAddAttachmentRequest), 201)]
+        public async Task<IActionResult> MobileBackEndUpdateVehicle([FromBody]MBESVRUpdateVehicle xmlInput)
+        {
+            try
+            {
+                if (xmlInput == null)
+                {
+                    string msg = string.Format("Invalid input data received <{0}>.  Verify input request data.", xmlInput);
+                    return BadRequest(msg);
+                }
+                var xmlOutput = xmlInput.ToXml("Request");//await falconPort.SubmitFalconRequest(aspPage, xmlInput.ToXml("Request"));
+                return StatusCode(StatusCodes.Status201Created, xmlInput);
+            }
+            catch (Exception ex)
+            {
+                string msg = string.Format("Processing error <{0}> received for PostAttachments request <{1}>.  Verify input request data.", ex.Message, xmlInput);
+                return BadRequest(msg);
+            }
+        }
+    }
+}
---
+++ ADXETools/Model/MBESVRAddAttachment.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace ADXETools.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [XmlRoot]
+    public class MBESVRAddAttachmentRequest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MBESVRAddAttachmentRequest() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [BindNever, XmlAttribute, JsonIgnore]
+        public string Method { get; set; } = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required, XmlElement]
+        public string WorkAssignmentId { get; set; } = string.Empty;
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required, XmlElement]
+        public string CreatedForProfileId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required, XmlElement]
+        public string RoleCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required, XmlElement]
+        public string BusinessCategory { get; set; } = string.Empty;                
+    }
+}
